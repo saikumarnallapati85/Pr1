4843d3850d50af28ab17037afec06481695f5f7d
Nallapati25 <nallapati.3@wright.edu>
2025-10-21 21:55:34 -0400

    Day 2.2 ReverseString

---
+++ src/ReverseString.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+
+        // Loop through the string in reverse order
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reversed);
+
+        scanner.close();
+    }
+}
+
