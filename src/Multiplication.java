fc349752825cc75c2b2906a21b9cf89e15a6e514
Nallapati25 <nallapati.3@wright.edu>
2025-10-23 10:55:32 -0400

    Multtiplication table

---
+++ src/Multiplication.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to print its multiplication table: ");
+        int num = scanner.nextInt();
+
+        System.out.println("Multiplication Table of " + num + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " × " + i + " = " + (num * i));
+        }
+
+        scanner.close();
+    }
+}
